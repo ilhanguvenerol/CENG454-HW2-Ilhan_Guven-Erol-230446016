9ddd8a3cf43a0aa20a6cbce1173b572087923cc7
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-04-02 23:42:53 +0300

    Template for FlightExamManager

---
+++ homework 1/Assets/Scripts/FlightExamManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+public class FlightExamManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text statusText;
+    [SerializeField] private TMP_Text missionText;
+
+    private bool hasTakenOff = false;
+    private bool threatCleared = false;
+    private bool missionComplete = false;
+
+    public void EnterDangerZone()
+    {
+        // TODO: update the mission state and HUD 
+    }
+
+    public void ExitDangerZone()
+    {
+        // TODO: mark the threat as cleared and refresh the HUD 
+    }
+}
