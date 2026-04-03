912f6e9cce6e73fef044cafe336a8150cdfc04fd
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-04-03 23:44:55 +0300

    methods for HUD controls

--- homework 1/Assets/Scripts/HUDController.cs
+++ homework 1/Assets/Scripts/HUDController.cs
@@ -1,10 +1,31 @@
 using UnityEngine;
 using TMPro;
+
 public class HUDController : MonoBehaviour
 {
     [SerializeField] private TMP_Text warningText;
     [SerializeField] private TMP_Text statusText;
     private void Awake()
     {
+        ClearWarning();
+        SetStatus("Clear for Takeoff");
+    }
+
+    public void ShowWarning(string message)
+    {
+        if (warningText != null)
+            warningText.text = message;
+    }
+
+    public void ClearWarning()
+    {
+        if (warningText != null)
+            warningText.text = "";
+    }
+
+    public void SetStatus(string message)
+    {
+        if (statusText != null)
+            statusText.text = message;
     }
 }
