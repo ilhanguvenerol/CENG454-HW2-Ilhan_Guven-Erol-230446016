624fb94557a91488ebbeeffdce25f46d9c34c509
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-04-03 23:44:12 +0300

    Separate script for hud controls

---
+++ homework 1/Assets/Scripts/HUDController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using TMPro;
+public class HUDController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text warningText;
+    [SerializeField] private TMP_Text statusText;
+    private void Awake()
+    {
+    }
+}
