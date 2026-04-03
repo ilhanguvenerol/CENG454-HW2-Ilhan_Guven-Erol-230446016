8cb1cf754daefde42a6d6f627ec2b20ade886cef
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-04-03 18:11:34 +0300

    Danger Zone behaviour

--- homework 1/Assets/Scripts/DangerZoneController.cs
+++ homework 1/Assets/Scripts/DangerZoneController.cs
@@ -1,24 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 public class DangerZoneController : MonoBehaviour
 {
     [SerializeField] private FlightExamManager examManager;
-    //[SerializeField] private MissileLauncher missileLauncher;
+    [SerializeField] private MissileLauncher missileLauncher;
     [SerializeField] private float missileDelay = 5f;
 
     private Coroutine activeCountdown;
 
-    private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter(Collider other)
     {
-        // TODO: confirm the Player tag 
-        // TODO: push the warning message "Entered a Dangerous Zone!" to the HUD 
+        // TODO: confirm the Player tag
+        if (!other.CompareTag("Player")) return;
+        // TODO: push the warning message "Entered a Dangerous Zone!" to the HUD
+        examManager.EnterDangerZone();
         // TODO: start the delayed missile launch countdown 
+        if (activeCountdown != null)
+            StopCoroutine(activeCountdown);//prevent duplicate countdowns
+
+        activeCountdown = StartCoroutine(MissileCountdown(other.transform));//MissileCountdown will be referenced from examManager, yet to be implemented
     }
 
-    private void OnTriggerExit(Collider collision)
+    private void OnTriggerExit(Collider other)
     {
         // TODO: confirm the Player tag 
-        // TODO: cancel any pending launch countdown 
+        if (!other.CompareTag("Player")) return;
+        // TODO: cancel any pending launch countdown
+        if (activeCountdown != null)
+        {
+            StopCoroutine(activeCountdown);
+            activeCountdown = null;
+        }
         // TODO: destroy the active missile and clear the HUD warning 
+        missileLauncher.DestroyActiveMissile();
+        examManager.ExitDangerZone();
     }
 }
