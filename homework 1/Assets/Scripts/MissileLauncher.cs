3a9b4285010d4d96649b757a84095a0476d9d4c6
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-04-05 19:28:35 +0300

    instantiate prefab for missile

--- homework 1/Assets/Scripts/MissileLauncher.cs
+++ homework 1/Assets/Scripts/MissileLauncher.cs
@@ -9,7 +9,8 @@ public class MissileLauncher : MonoBehaviour
 
     public GameObject Launch(Transform target)
     {
-        // TODO (Task 3-A): instantiate the missile at launchPoint 
+        // TODO (Task 3-A): instantiate the missile at launchPoint
+        activeMissile = Instantiate(missilePrefab, launchPoint.position, launchPoint.rotation);
         // TODO (Task 3-B): give the missile its target 
         // TODO (Task 3-C): play launch audio and return the spawned missile 
         return null;
