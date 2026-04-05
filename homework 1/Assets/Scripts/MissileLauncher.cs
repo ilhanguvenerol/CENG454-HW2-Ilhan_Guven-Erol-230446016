1d9ec67c6d6489086c6166cf83a22313e7cdb67a
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-04-05 19:29:40 +0300

    set hte target for missile

--- homework 1/Assets/Scripts/MissileLauncher.cs
+++ homework 1/Assets/Scripts/MissileLauncher.cs
@@ -17,6 +17,12 @@ public class MissileLauncher : MonoBehaviour
         // TODO (Task 3-A): instantiate the missile at launchPoint
         activeMissile = Instantiate(missilePrefab, launchPoint.position, launchPoint.rotation);
         // TODO (Task 3-B): give the missile its target 
+        MissileHoming homing = activeMissile.GetComponent<MissileHoming>();
+        if (homing != null)
+            homing.SetTarget(target);
+        else
+            Debug.LogWarning("[MissileLauncher] Missile prefab is missing MissileHoming component.");
+
         // TODO (Task 3-C): play launch audio and return the spawned missile 
         return null;
     }
